2917ed2a680ac1e66f287c4959edbf8e7b1302b0
Seba <belhachem@gmail.com>
2020-02-27 16:59:01 -0500

    Fixing spaces and commenting

--- src/Permissions/Permission.php
+++ src/Permissions/Permission.php
@@ -6,8 +6,10 @@ use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
 {
+	//Since we are using the DeleteLink component in the Catalog,
+	//we extend Spatie's Permission model to set the deletable authorization logic.
     public function deletable()
-	{
-		return auth()->user() && auth()->user()->hasRole('admin|super-admin');
-	}
+    {
+        return auth()->user() && auth()->user()->hasRole('admin|super-admin');
+    }
 }
